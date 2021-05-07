a7fb74b64e62d6486fd889a48d35c720ae2ab8d9
Shrey Dixit <shrey.dixit.17cse@bml.edu.in>
2021-05-07 10:10:48 +0530

    Passed 10 Random multiple digit numbers test cases

--- src/main/StringCalculatorTest.java
+++ src/main/StringCalculatorTest.java
@@ -2,14 +2,25 @@ package main;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringCalculatorTest {
+    Random rd;
+
+    public StringCalculatorTest() {
+        this.rd = new Random();
+    }
 
     @Test
     public void TestAdd() {
         assertEquals(0, StringCalculator.Add(""));
         assertEquals(1, StringCalculator.Add("1"));
         assertEquals(2, StringCalculator.Add("2"));
+        for (int i=0; i<10; i++) {
+            int rand = rd.nextInt();
+            assertEquals(rand, StringCalculator.Add(String.valueOf(rand)));
+        }
     }
 }
