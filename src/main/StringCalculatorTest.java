bb3c0df332dec0d2891eb252dbb87fa665babef3
Shrey Dixit <shrey.dixit.17cse@bml.edu.in>
2021-05-07 10:01:03 +0530

    Passed "2"->2 Test Case

--- src/main/StringCalculatorTest.java
+++ src/main/StringCalculatorTest.java
@@ -10,5 +10,6 @@ class StringCalculatorTest {
     public void TestAdd() {
         assertEquals(0, StringCalculator.Add(""));
         assertEquals(1, StringCalculator.Add("1"));
+        assertEquals(2, StringCalculator.Add("2"));
     }
 }
